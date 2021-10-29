3d6afcd537062fbe35ec60604328e863c5e4ee97
Pierre Caillet <pierrE.caillet@edu.devinci.fr>
2021-10-29 17:08:31 +0200

    :lipstick: 💫 Add style elements and animations home page

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ window.onscroll = function() {scroling_effect()}
 function scroling_effect() {
 if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     header.style.boxShadow = "inset 0px -4px 8px rgba($color: #110F0E, $alpha: 0.1)"
-    console.log()
 } else{
     header.style.boxShadow = ""
 }
+}
+
+function tp_top() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+function tp_lower() {
+    window.scrollTo({top: 1000, behavior: 'smooth'});
 }
